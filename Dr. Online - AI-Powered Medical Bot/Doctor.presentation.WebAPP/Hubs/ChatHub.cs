edae7f62b47563617212d530e5f3f7ca3eb14813
Hager sayed <150134581+Hagersayed612@users.noreply.github.com>
2024-10-25 00:42:24 +0300

    Add files via upload

---
+++ Dr. Online - AI-Powered Medical Bot/Doctor.presentation.WebAPP/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Doctor.Presentation.WebApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string fromUser, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, message);
+        }
+
+    }
+}
