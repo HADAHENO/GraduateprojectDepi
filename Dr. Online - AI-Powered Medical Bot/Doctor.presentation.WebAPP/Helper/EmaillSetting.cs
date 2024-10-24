e75e04850ebf81590a78f326f3d50acc5bc28a3b
Hager sayed <150134581+Hagersayed612@users.noreply.github.com>
2024-10-25 00:41:57 +0300

    Add files via upload

---
+++ Dr. Online - AI-Powered Medical Bot/Doctor.presentation.WebAPP/Helper/EmaillSetting.cs
@@ -0,0 +1,19 @@
+﻿using Mono.TextTemplating;
+using Doctor.Model.App;
+using System.Net.Mail;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using System.Net;
+namespace Doctor.presentation.WebAPP.Helper
+{
+    public   static class EmaillSetting
+    {
+        public static void SendEmail(Email email)
+        {
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.EnableSsl = true;  
+            smtpClient.Credentials = new NetworkCredential("hagersayed573@gmail.com", "ygmq cajy vypk bhhi");
+            smtpClient.Send(email.From,email.To,email.Title,email.Body);
+        }
+    }
+}
+  
