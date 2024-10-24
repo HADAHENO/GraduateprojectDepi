5967b113806905e9a7145b7d84d47619e8607d85
Hager sayed <150134581+Hagersayed612@users.noreply.github.com>
2024-10-25 00:25:38 +0300

    Add files via upload

---
+++ Dr. Online - AI-Powered Medical Bot/Doctor.Data.APP/DoctorContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Doctor.Model.App;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+namespace Doctor.Data.APP
+{
+    public class DoctorContext : IdentityDbContext<ApplicationUser,IdentityRole<int>,int>
+    {
+        public DbSet<Response> Responses { get; set; }
+    public DbSet<Symptoms> Symptoms { get; set; }
+        public DoctorContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
